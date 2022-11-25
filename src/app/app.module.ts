e5ea2be72612128e5e73af1f574cbe00f7857ce1
Abraham-Jerin-John <abrahamkalarickal98@gmail.com>
2022-11-25 09:39:23 +0530

    budfix

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,18 +7,39 @@ import { ArabianComponent } from './arabian/arabian.component';
 import { ChineseComponent } from './chinese/chinese.component';
 import { NorthIndianComponent } from './north-indian/north-indian.component';
 import { SouthIndianComponent } from './south-indian/south-indian.component';
-
+import { NavbarComponent } from './navbar/navbar.component';
+import { RouterModule, Routes } from '@angular/router';
+const myRoute:Routes=[
+  {
+    path:"",
+    component:ArabianComponent
+  },
+  {
+    path:"chinese",
+    component:ChineseComponent
+  },
+  {
+    path:"northindian",
+    component:NorthIndianComponent
+  },
+  {
+    path:"southindian",
+    component:SouthIndianComponent
+  }
+]
 @NgModule({
   declarations: [
     AppComponent,
     ArabianComponent,
     ChineseComponent,
     NorthIndianComponent,
-    SouthIndianComponent
+    SouthIndianComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(myRoute)
   ],
   providers: [],
   bootstrap: [AppComponent]
