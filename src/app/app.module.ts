51cc310d3d5522a390baf24b3b17f24b65b2d4a7
Abraham-Jerin-John <abrahamkalarickal98@gmail.com>
2022-11-25 09:05:50 +0530

    chinese

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ArabianComponent } from './arabian/arabian.component';
+import { ChineseComponent } from './chinese/chinese.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ArabianComponent
+    ArabianComponent,
+    ChineseComponent
   ],
   imports: [
     BrowserModule,
